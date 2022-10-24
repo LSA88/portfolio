93588a7bddeefe094f0bf0feb447855f174ee88a
LSA88 <lovervz@naver.com>
2022-10-24 14:09:53 +0900

    JS : Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
@@ -12,34 +11,38 @@ document.addEventListener("scroll", () => {
   }
 });
 
-// Handle scolling when tappping on the navbar menu
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
-
 navbarMenu.addEventListener("click", event => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove("open");
   scrollIntoView(link);
 });
 
-// Handle click on "contact me" button on home
-const homeContact = document.querySelector(".home__contact");
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
 
-homeContact.addEventListener("click", event => {
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
 // Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
-
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-// Show "arrow up" button when scrollling down
+// Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
   if (window.scrollY > homeHeight / 2) {
@@ -66,14 +69,15 @@ workBtnContainer.addEventListener("click", e => {
 
   // Remove selection from the previous item and select the new one
   const active = document.querySelector(".category__btn.selected");
-  active.classList.remove("selected");
-  const target =
-    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
-  target.classList.add("selected");
+  if (active != null) {
+    active.classList.remove("selected");
+  }
+  e.target.classList.add("selected");
 
   projectContainer.classList.add("anim-out");
   setTimeout(() => {
     projects.forEach(project => {
+      console.log(project.dataset.type);
       if (filter === "*" || filter === project.dataset.type) {
         project.classList.remove("invisible");
       } else {
